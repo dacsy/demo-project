d44f8485b54fbc47283318606fd59bacdbe4637c
dacsyle <dacsyle@gmail.com>
2020-11-02 15:16:12 +0700

    Initialize project demo
    Initialize project demo

---
+++ src/test/java/remitano/dacsyle/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package remitano.dacsyle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class DemoApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
